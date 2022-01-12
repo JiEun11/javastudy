9591e3fbf0c1a766152cf6b570960a2b299230f0
BIT <BIT@DESKTOP-MIG2BMV>
2022-01-12 12:26:44 +0900

    22.01.12 helloworld2 추가 & pom.xml 설정

---
+++ helloworld2/src/main/java/com/poscoict/helloworld/hello/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.poscoict.helloworld.hello;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloWorld {
+
+	public static int count = 10;
+	
+	public static void main(String[] args) {
+		hello();
+		System.out.println("Hello World");
+		
+	}
+	
+	public static void hello() {
+		Map<String, Object> m = new HashMap<>();
+	}
+
+}
