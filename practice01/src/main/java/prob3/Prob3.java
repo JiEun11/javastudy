61d2a27d270b2522d47148b1c6de6fb1880fd9a8
Bella <shdudtnr3939@naver.com>
2022-01-14 08:57:48 +0900

    thanks~

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -9,7 +9,28 @@ public class Prob3 {
 		Scanner scanner = new Scanner(System.in);
 
 		/* 코드 작성 */
+		System.out.print("숫자를 입력하세요: ");
+		int num = scanner.nextInt();
+		int result = 0;
+		result = sumNums(num);
 		
+		System.out.println("결과값 : " + result);
 		scanner.close();
+	}
+
+	private static int sumNums(int num) {
+		int sum = 0;
+		
+		if(num%2==1) {	// num이 홀수 일 때 
+			for(int i = 1; i<= num; i += 2) {
+				sum += i;
+			}
+			
+		}else {		// num이 짝수 일 때 
+			for(int i = 0; i<= num; i += 2) {
+				sum += i;
+			}
+		}
+		return sum;
 	}
 }
