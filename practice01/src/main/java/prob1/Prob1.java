f14c8b2f007fd0545c0589abbe07760cfe804354
Bella <shdudtnr3939@naver.com>
2022-01-14 15:27:37 +0900

    22.01.14 prac1-prob1

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -1,10 +1,28 @@
 package prob1;
 
+import java.util.Scanner;
+
 public class Prob1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("수를 입력하세요: ");
+		int num = scan.nextInt();
+		
+		ifMultiple(num);
+	}
 
+	private static void ifMultiple(int num) {
+		
+		if(num%3==0) {
+			System.out.println("3의 배수입니다.");
+			
+		}else {
+			System.out.println("3의 배수가 아닙니다.");
+		}
+		
 	}
 
 }
