0b89a35095697b72f070bea251b71ebaf5e4957a
Bella <shdudtnr3939@naver.com>
2022-01-15 13:22:54 +0900

    22.01.15 practice03 prob1-6 init

---
+++ practice03/src/main/java/prob01/Member.java
@@ -0,0 +1,29 @@
+package prob01;
+
+public class Member {
+	
+	private int id;			// 회원 id
+	private String name;	// 회원 이름
+	private int point;		// 회원 point
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+	
+}
---
+++ practice03/src/main/java/prob02/Goods.java
@@ -0,0 +1,28 @@
+package prob02;
+
+public class Goods {
+	private String product;
+	private int price;
+	private int countStock;
+	
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getcountStock() {
+		return countStock;
+	}
+	public void setcountStock(int countStock) {
+		this.countStock = countStock;
+	}
+	
+	
+}
