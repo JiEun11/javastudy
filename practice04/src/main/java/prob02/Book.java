d3bb050cacde8e20bf5ef548e95098119688c28a
Bella <shdudtnr3939@naver.com>
2022-01-16 14:06:17 +0900

    22.01.16 prac4 prob2

---
+++ practice04/src/main/java/prob02/Book.java
@@ -0,0 +1,63 @@
+package prob02;
+
+public class Book {
+	private int bookNo;		//책 번호
+	private String title;	// 책 제목
+	private String author;	// 작가
+	private int stateCode;	// 대여 유무 상태 코드
+	
+	Book(int bookNo, String title, String author){
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+		this.stateCode = 1;
+	}
+	
+	public int getBookNo() {
+		return bookNo;
+	}
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getStateCode() {
+		return stateCode;
+	}
+	public void setStateCode(int stateCode) {
+		this.stateCode = stateCode;
+	}
+	
+	/*
+	 * 	대여기능 수행
+	 */
+	public void rent() {
+		if(getStateCode()==1) {
+			System.out.println(getTitle() + " 이(가) 대여 되었습니다.");
+			setStateCode(0);
+		}else {
+			System.out.println(getTitle() + " 이(가) 이미 대여 중입니다.");
+		}
+	}
+	
+	/*
+	 * 책의 정보 출력 
+	 */
+	public void print() {
+		String ifStock ="재고 있음";
+		if(getStateCode()==0) {
+			ifStock = "대여중";
+		}
+		System.out.printf("책 제목: %s, 작가: %s, 대여 유무: %s %n", getTitle(), getAuthor(), ifStock);
+	}
+}
