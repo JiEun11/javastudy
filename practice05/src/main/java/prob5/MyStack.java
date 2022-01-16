0dae24b76100319dc937a76a09b66eab052424ff
Bella <shdudtnr3939@naver.com>
2022-01-16 23:55:27 +0900

    22.01.16 prac5 prob5

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -1,4 +1,29 @@
 package prob5;
 
 public class MyStack {
+	private String[] stack = null;
+	private int top = -1;
+	
+	MyStack(int num){
+		stack = new String[num];
+	}
+	
+	public void push(String str) {
+		stack[++top] = str;
+//		System.out.println(top);
+	}
+
+	public String pop() throws MyStackException  {
+		if(this.isEmpty() == false) {
+			return stack[top--];
+		}else {
+			throw new MyStackException("stack is empty");
+		}
+	}
+
+	public boolean isEmpty() {
+		if(stack == null || this.top == -1) {
+			return true;
+		}else return false;
+	}
 }
