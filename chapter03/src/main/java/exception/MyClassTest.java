8f4ed23b64bfbb07a3962a5fe62fd3ecf004d8e2
Bella <shdudtnr3939@naver.com>
2022-01-25 12:25:20 +0900

    22.01.25 exception 실습

---
+++ chapter03/src/main/java/exception/MyClassTest.java
@@ -0,0 +1,22 @@
+package exception;
+
+import java.io.IOException;
+
+public class MyClassTest {
+
+	public static void main(String[] args) {
+
+		try {
+			MyClass mc = new MyClass();
+			mc.danger();
+			
+		} catch (IOException e) {
+			System.out.println("error: " + e);
+			
+		} catch (MyException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
